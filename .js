994f3223e295bafb9bedc2065b72b2ccb838d987
Zyproxd <alexmartini0802@gmail.com>
2023-02-21 18:56:01 -0500

    Coreball

---
+++ .js
@@ -0,0 +1,6 @@
+$('#jsbutton').click(function(){
+    alert('This button uses javascipt.');
+  });
+  $('#jsbutton2').click(function(){
+    alert('This button uses javascipt, too. Javascipt is integrated into this website.');
+  });
